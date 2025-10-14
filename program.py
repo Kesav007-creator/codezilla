ff505c0d76884b2555f8ac22e49f3240b18bc064
Kesav007-creator <kesavsankar007@gmail.com>
2025-10-14 21:50:12 +0530

    program

---
+++ program.py
@@ -0,0 +1,39 @@
+#TASK 4 : DSA FUNDAMENTALS(PYTHON)
+
+#D1 : count how many times 'l' appears in "hello world"
+print("count how many times 'l' appears in 'hello world'")
+a = "hello world"
+count=0
+for i in a:
+    if i =="l":
+        count+=1
+print("number of times 'l' appeared in 'hello world' is -",count)
+
+#D2 : find the smallest value in an array of 8 integers
+print("\nfind the smallest value in an array of 8 integers")
+l=[]
+for i in range(8):
+    a=int(input("enter number >"))
+    l.append(a)
+min =l[0]
+for i in range(len(l)):
+    if min> l[i]:
+        min=l[i]
+print("\nthe smallest value in the array is",min)
+
+
+#D3 : manually find the 2nd largest number in an array
+print("\n manually find the 2nd largest number in an array")
+l=[]
+n = int(input("enter number of elements u want to enter into array"))
+for i in range(n):
+    a=int(input("enter number >"))
+    l.append(a)
+print(l)
+max=l[0]
+sec=l[0]
+for i in range(len(l)):
+    if max<l[i]:
+        sec=max
+        max=l[i]
+print("the second largest number in an array is : ",sec)
